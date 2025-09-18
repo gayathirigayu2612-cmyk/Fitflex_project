555281a9e3d8c68d330cdd21adaa9bea404174c4
gayathirigayu2612-cmyk <gayathirigayu2612@gmail.com>
2025-09-18 14:43:29 +0530

    Create Setup tests.js

---
+++ Setup tests.js
@@ -0,0 +1,5 @@
+https://github.com/bhuvaneshwaribhuna83-dot/FitFlex-Your-Personal-Fitness-Companion/blob/main/logo.svgjest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
